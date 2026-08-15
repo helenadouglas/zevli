30d14daa00c34c41eaf8f442156969489729c61f
helenadouglas <helena.m.douglas02@gmail.com>
2026-08-15 10:09:40 +0200

    Initial Commit

---
+++ VitalsTrack/VitalsTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalsTrackApp.swift
+//  VitalsTrack
+//
+//  Created by Helena Douglas on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct VitalsTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
